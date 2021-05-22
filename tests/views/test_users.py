c8d445991c695c8594e9eea5b35bd0f034f4dd1d
hubert <hubbertt.k@gmail.com>
2021-05-22 17:30:21 +0200

    created tests for the searchForBook function.

--- tests/views/test_users.py
+++ tests/views/test_users.py
@@ -167,7 +167,120 @@ class TestUsers:
         assert '_schema' in response.json
         assert response.json['_schema'][0] == "No title or author."
 
+
+    def test_searchForBooks_400_Both_Fields_Missing(self, client, normal_access_token):
+        """
+        Test the searchForBooks function.
+
+            If both fields are missing:
+        :assert: response status code is 400.
+        :assert: '_schema' error message is sent in response.
+        """
+        payload = {
+
+        }
+
+        response = client.post(
+            '/users/searchBooks',
+            headers={
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer ' + normal_access_token
+            },
+            data = json.dumps(payload)
+        )
+
+        assert response.status_code == 400
+        assert '_schema' in response.json
+        assert response.json['_schema'][0] == "No title or author."
+
     
+    def test_searchForBooks_Strip_Whitespaces(self, client, normal_access_token, db_populate_books):
+        """
+        Test the searchForBooks function.
+
+            If in request there are leading and trailing whitespaces:
+        :assert: strips leading and trailing whitespaces
+        :assert: api returns a proper response.
+        """
+
+        payload = {
+            "title": " book   ",
+            "author":"2"
+        }
+
+        response = client.post(
+            '/users/searchBooks',
+            headers={
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer ' + normal_access_token
+            },
+            data = json.dumps(payload)
+        )
+
+        assert response.status_code == 200
+        assert response.json
+
+        keys_expected = ['author','id','isReserved','pages','title']
+        response_keys = [x for x in response.json[0].keys()]
+
+        assert set(keys_expected).issubset(set(response_keys))
+
+        #Strip the fields and search for books.
+        book_title = "%{}%".format(payload['title'].strip())
+        book_author = "%{}%".format(payload['author'].strip())
+        books_expected = Book.query.filter(
+            Book.title.like(book_title), 
+            Book.author.like(book_author)
+        ).all()
+
+        assert len(response.json) == len(books_expected)
+
+    
+    def test_searchForBooks_400_One_Field_AND_Empty(self, client, normal_access_token):
+        """
+        If there is only one field filled and it is empty:
+        :assert: response status code is 400.
+        :assert: schema errors are sent in response.
+        """
+        payload = {
+            "title": "",
+        }
+        response = client.post(
+            '/users/searchBooks',
+            headers={
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer ' + normal_access_token
+            },
+            data = json.dumps(payload)
+        )
+        
+        assert response.status_code == 400
+        assert '_schema' in response.json
+        assert response.json['_schema'][0] == "No title or author."  
+
+
+    def test_searchForBooks_400_One_Field_AND_Only_Whitespaces(self, client, normal_access_token):
+        """
+        It there is only one field filled and consists of only whitespaces.
+        :assert: response status code is 400.
+        :assert: schema errors are sent in response.
+        """
+        payload = {
+            "title": "      ",
+        }
+        response = client.post(
+            '/users/searchBooks',
+            headers={
+                'Content-Type':'application/json',
+                'Authorization': 'Bearer ' + normal_access_token
+            },
+            data = json.dumps(payload)
+        )
+        
+        assert response.status_code == 400
+        assert '_schema' in response.json
+        assert response.json['_schema'][0] == "No title or author." 
+
     
     def test_searchForBooks_401_Token_Missing(self, client):
         """
